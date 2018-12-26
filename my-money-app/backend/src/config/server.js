b7b91bd809f9b25bc3f9d925623d6f80789549c7
Matheus <westsandro@MacBook-Air-de-west.local>
2018-12-26 13:23:44 -0300

    concluindo backend da applicacao

--- my-money-app/backend/src/config/server.js
+++ my-money-app/backend/src/config/server.js
@@ -3,9 +3,13 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const allowerCors = require('./cors')
+const queryParser = require('express-query-int')
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowerCors)
+server.use(queryParser())
 
 server.listen(port, function(){
     console.log(`BACKEND is running on port ${port}`)
